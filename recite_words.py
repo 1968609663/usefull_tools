4b396e68e70ae02305312c97549a4ba19d9683e6
1968609663 <1968609663@qq.com>
2018-09-24 06:53:24 +0800

    python实现自定义词典
    可以用于记录单词，命令等方便复习；
    词库文件使用的是 argparse ，可以注释，使其失效；
    可以指定单次复习时候每个词的初始次数，识别错误则+1，识别正确则-1，当为0时候，该词不再出现；
    程序退出时候，会生成文件提示错误的词；
    可以配置不同的section，标识不同内容的专业术语；

---
+++ recite_words.py
@@ -0,0 +1,113 @@
+# coding:utf-8
+"""
+author: allen
+summary: 用于背单词或者命令等
+email: 1902901293@qq.com
+python version: python3.x
+运行：
+    cmd命令行输入： python recite_words.py
+"""
+import argparse
+import configparser
+import os
+import random
+from itertools import chain
+
+
+class ReciteWords(object):
+    def __init__(self):
+        self.dir_p = os.path.dirname(os.path.abspath(__file__))
+        self.err_info = {}
+        self.args = self.parse_command()
+        self.filename = self.check_path(self.args.filename)
+        self.out = self.check_path(self.args.output)
+
+    def check_path(self, fn):
+        res = None
+        if os.path.isabs(fn) and (os.path.exists(fn)):
+            res = fn
+        else:
+            res = os.path.join(self.dir_p, os.path.basename(fn))
+        if not os.path.exists(res):
+            print(u"文件路径指定不准确: {}".format(res))
+        return res
+
+    def save(self):
+        wd = []
+        err_seq = list(self.err_info.items())
+        print(err_seq)
+        err_seq.sort(key=lambda x: x[1], reverse=True)
+        for k, v in err_seq:
+            wd.append(u"{} >>> {}:{}    错误次数: {}".format(k[0], k[1], v[1], v[0]))
+        res = '\n'.join(wd)
+        print(u'=' * 10, u'需要复习如下如下', u'=' * 10)
+        print(res)
+        with open(self.out, 'w', encoding='utf-8') as fp:
+            fp.write(res)
+        print(u'错误记录已经保存在文件: {}'.format(self.out))
+
+    def run(self):
+        data = self.parse_conf()
+        w_infos = {k: self.args.weight for k, _ in data.items()}
+        while 1:
+            seq = list(chain(*[[k] * w_infos[k] for k, _ in data.items()]))
+            if not seq:
+                raise KeyboardInterrupt
+            key = random.choice(seq)
+            notice = random.choice(data[key])
+            u = input(u"{s}  :  '{n}'  对应>>>".format(s=key[0], n=notice))
+            if u.lower() == "quit!":
+                raise KeyboardInterrupt
+            if u.strip() != key[1]:
+                print(u"正确关系: '{c}'<<-->>'{n}'".format(c=key[1], n=notice))
+                w_infos[key] += 1
+                if key not in self.err_info:
+                    self.err_info[key] = [0, None]
+                    self.err_info[key][0] = 0
+                    self.err_info[key][1] = notice
+                self.err_info[key][0] += 1
+
+            elif w_infos[key] >= 1:
+                w_infos[key] -= 1
+            print()
+
+    def parse_conf(self):
+        """
+        解析词库文件
+        :return:
+        """
+        data = {}
+        conf = configparser.ConfigParser()
+        try:
+            conf.read(self.filename, encoding='gbk')
+        except UnicodeDecodeError:
+            conf.read(self.filename, encoding='utf-8')
+        sec = conf.sections()
+        for se in sec:
+            data.update({(se, k): v.split(',') for k, v in conf[se].items()})
+        return data
+
+    def parse_command(self):
+        """
+        解析命令行参数
+        :return:
+        """
+        parser = argparse.ArgumentParser(description="自定义单词或者命令复习,词典文件的词条可以用 '#' 注释，配置值的分隔符为 ',' run: python recite_words.py ")
+        parser.add_argument('--filename', help=u'指定自定义词典文件的路径,默认为脚本所在同级目录,默认为: words.ini',
+                            default='words.ini')
+        parser.add_argument('--output', help=u'输出本次运行过程中出现错误的记录，默认文件为: need_enhance_words.txt',
+                            default='need_enhance_words.txt')
+        parser.add_argument('--weight', help="自定义词的初始权重,为整数，当回答正确的会被减1，否则加1，每个词对应权重，当权重为0时，该词不会再出现",
+                            default=5, type=int)
+        args = parser.parse_args()
+        return args
+
+
+if __name__ == '__main__':
+
+    work = ReciteWords()
+    try:
+        work.run()
+    except (KeyboardInterrupt, KeyboardInterrupt, Exception):
+        print("退出程序")
+    work.save()
